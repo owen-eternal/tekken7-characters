ff84b770799e67c1d8080d1caab5635a628f675a
owen-eternal <ptarworx@gmail.com>
2023-08-24 02:26:58 +0200

    Test list viewset for status 200

--- characters/tests/test_views.py
+++ characters/tests/test_views.py
@@ -8,3 +8,7 @@ class CharacterViewSetTestCase(APITestCase):
     def setUp(self):
         character_1 = Character.objects.create(name='Noctis')
         character_2 = Character.objects.create(name='Bob')
+        
+    def test_character_list_statuscode_200(self):
+        response = self.client.get(reverse("character-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

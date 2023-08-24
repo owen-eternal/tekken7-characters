6bdbe1aed0c71b1c95cf51d35ca44efe1f3442e4
owen-eternal <ptarworx@gmail.com>
2023-08-24 02:17:55 +0200

    Create test case for CharacterViewSet

---
+++ characters/tests/test_views.py
@@ -0,0 +1,7 @@
+from django.urls import reverse
+from characters.models import Character
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+class CharacterViewSetTestCase(APITestCase):
+    pass

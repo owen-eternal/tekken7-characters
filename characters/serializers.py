48170ae8aa5e0cb897c6f8ec43a7b1a339e5c056
owen-eternal <ptarworx@gmail.com>
2023-08-27 01:33:30 +0200

    Fix linting

--- characters/serializers.py
+++ characters/serializers.py
@@ -1,23 +1,33 @@
 from rest_framework import serializers
 from .models import Character, Profile, Ability, Specialty
 
+
 class ProfileSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Profile
         fields = ['character', 'description']
+
 
 class AbilitySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ability
-        fields = ['character', 'combo_damage', 'mobility', 'wall_carry', 'throw_game']
-    
+        fields = [
+            'character',
+            'combo_damage',
+            'mobility',
+            'wall_carry',
+            'throw_game'
+        ]
+
+
 class SpecialtySerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Specialty
         fields = ('character', 'spec_name', 'damage', 'move_list')
+
 
 class CharacterSerializer(serializers.ModelSerializer):
 
@@ -27,4 +37,4 @@ class CharacterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Character
-        fields = '__all__'        
+        fields = '__all__'

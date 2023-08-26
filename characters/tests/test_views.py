22c43ee2ed53df82eed5bda6e83b0cafe2d565b8
owen-eternal <ptarworx@gmail.com>
2023-08-26 23:46:12 +0200

    update testsuit

--- characters/tests/test_views.py
+++ characters/tests/test_views.py
@@ -1,27 +1,26 @@
 from django.test import TestCase
 from django.urls import reverse
 from characters.models import Character
-from rest_framework import status
 
 class CharacterViewSetTestCase(TestCase):
 
     def setUp(self):
-        character_1 = Character.objects.create(name='Noctis')
-        character_2 = Character.objects.create(name='Bob')
+        self.character_1 = Character.objects.create(name='Noctis')
+        self.character_2 = Character.objects.create(name='Bob')
         
     def test_character_list_statuscode_200(self):
         response = self.client.get(reverse("character-list"))
         self.assertEqual(response.status_code, 200)
 
-    # def test_character_retrieve_statuscode_200(self):
-    #     response = self.client.get(reverse("character-detail", kwargs={"pk": 1}))
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data['name'], 'Noctis')
+    def test_character_retrieve_statuscode_200(self):
+        response = self.client.get(reverse("character-detail", args=[self.character_1.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'Noctis')
 
-    # def test_number_of_character_entries(self):
-    #     response = self.client.get(reverse("character-list"))
-    #     self.assertEqual(len(response.data), 2)
+    def test_number_of_character_entries(self):
+        response = self.client.get(reverse("character-list"))
+        self.assertEqual(len(response.data), 2)
 
-    # def test_number_of_data_fields(self):
-    #     response = self.client.get(reverse("character-detail", kwargs={"pk": 1}))
-    #     self.assertEqual(len(response.data), 5)
+    def test_number_of_data_fields(self):
+        response = self.client.get(reverse("character-detail", args=[self.character_1.id]))
+        self.assertEqual(len(response.data), 5)

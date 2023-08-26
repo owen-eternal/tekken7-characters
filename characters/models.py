7d3c96e0f34c08627d505fecea7f9fdbeb53aefd
owen-eternal <ptarworx@gmail.com>
2023-08-27 01:23:07 +0200

    Fix linting

--- characters/models.py
+++ characters/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 
+
 class Character(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
+
 class Profile(models.Model):
-    character = models.OneToOneField(Character, on_delete=models.CASCADE, primary_key=True, related_name='profile')
+    character = models.OneToOneField(
+        Character,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='profile'
+    )
     description = models.TextField()
 
     def __str__(self):
         return f'Profile for: {self.character.name}'
 
+
 class Ability(models.Model):
-    character = models.OneToOneField(Character, on_delete=models.CASCADE, primary_key=True, related_name='ability')
+    character = models.OneToOneField(
+        Character,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='ability'
+    )
     combo_damage = models.IntegerField()
     mobility = models.IntegerField()
     wall_carry = models.IntegerField()
@@ -23,11 +36,16 @@ class Ability(models.Model):
     def __str__(self):
         return f'Abilities for: {self.character.name}'
 
+
 class Specialty(models.Model):
-    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name='specialties')
+    character = models.ForeignKey(
+        Character,
+        on_delete=models.CASCADE,
+        related_name='specialties'
+    )
     spec_name = models.CharField(max_length=30)
     damage = models.CharField(max_length=30)
     move_list = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return f'Specialty for: {self.character.name}'
